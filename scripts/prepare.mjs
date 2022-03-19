2f8ff26d6445e7d0abd850f321e82810ca56f20d
Guillaume Hivert <hivert.is.coming@gmail.com>
2022-03-19 21:53:56 +0100

    ➕ Update svb-engine

--- scripts/prepare.mjs
+++ scripts/prepare.mjs
@@ -8,10 +8,12 @@ const replaceModuleNames = file => {
     .replace(/declare module "geometry/g, 'declare module "@svb-41/core/geometry')
     .replace(/declare module "radar/g, 'declare module "@svb-41/core/radar')
     .replace(/declare module "ship/g, 'declare module "@svb-41/core/ship')
+    .replace(/declare module "console/g, 'declare module "@svb-41/core/console')
     .replace(/export (.*) from "comm/g, 'export $1 from "@svb-41/core/comm')
     .replace(/export (.*) from "geometry/g, 'export $1 from "@svb-41/core/geometry')
     .replace(/export (.*) from "radar/g, 'export $1 from "@svb-41/core/radar')
     .replace(/export (.*) from "ship/g, 'export $1 from "@svb-41/core/ship')
+    .replace(/export (.*) from "console/g, 'export $1 from "@svb-41/core/console')
     .replace(/\/\/\#.*/, '')
 }
 
